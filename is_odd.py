9d4dc231cf6380a61d7d59c3d61f3080d956cfd3
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-09 15:11:58 +0500

    ikki0202

--- is_odd.py
+++ is_odd.py
@@ -1,7 +1,9 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is odd, otherwise print 0.
-number=5
+number=7
 print(1)
-number=5+1
+number=number+1
 print(0)
+
+
