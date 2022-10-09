83e25bbefa958cfaed7d9443b096bb0ca12e28c2
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-09 13:36:31 +0500

    birinchi

--- is_even.py
+++ is_even.py
@@ -1,3 +1,14 @@
 #Create a variable "number" and assign it a integer value.
 
-#Print 1 if the number is even, otherwise print 0.
+#Print 1 if the number is even, otherwise print
+number=20
+print(1)
+
+
+
+
+
+
+
+
+
